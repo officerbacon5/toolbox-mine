27fd2900094db3e6990a7515fe06f9af2e564969
officerbacon5 <37131825+officerbacon5@users.noreply.github.com>
2018-03-19 22:20:46 -0500

    Add files via upload

---
+++ cl_action.lua
@@ -0,0 +1,188 @@
+--[[------------------------------------------------------------------------
+
+	ActionMenu 
+	Created by WolfKnight
+	Additional help from lowheartrate, TheStonedTurtle, and Briglair. 
+
+------------------------------------------------------------------------]]--
+
+-- Define the variable used to open/close the menu 
+local menuEnabled = false 
+
+--[[------------------------------------------------------------------------
+	ActionMenu Toggle
+	Calling this function will open or close the ActionMenu. 
+------------------------------------------------------------------------]]--
+function ToggleActionMenu()
+	-- Make the menuEnabled variable not itself 
+	-- e.g. not true = false, not false = true 
+	menuEnabled = not menuEnabled
+
+	if ( menuEnabled ) then 
+		-- Focuses on the NUI, the second parameter toggles the 
+		-- onscreen mouse cursor. 
+		SetNuiFocus( true, true )
+
+		-- Sends a message to the JavaScript side, telling it to 
+		-- open the menu. 
+		SendNUIMessage({
+			showmenu = true 
+		})
+	else 
+		-- Bring the focus back to the game
+		SetNuiFocus( false )
+
+		-- Sends a message to the JavaScript side, telling it to
+		-- close the menu.
+		SendNUIMessage({
+			hidemenu = true 
+		})
+	end 
+end 
+
+--[[------------------------------------------------------------------------
+	ActionMenu HTML Callbacks
+	This will be called every single time the JavaScript side uses the
+	sendData function. The name of the data-action is passed as the parameter
+	variable data. 
+------------------------------------------------------------------------]]--
+RegisterNUICallback( "ButtonClick", function( data, cb ) 
+	if ( data == "toggle cuff" ) then 
+		TriggerEvent( "Toggle cuff" )
+	elseif ( data == "button2" ) then 
+		chatPrint( "Button 2 pressed!" )
+	elseif ( data == "button3" ) then 
+		chatPrint( "Button 3 pressed!" )
+	elseif ( data == "kneel" ) then 
+		TriggerEvent( "Kneelhu" )
+	elseif ( data == "exit" ) then 
+		-- We toggle the ActionMenu and return here, otherwise the function 
+		-- call below would be executed too, which would just open the menu again 
+		ToggleActionMenu()
+		return 
+	end 
+
+	-- This will only be called if any button other than the exit button is pressed
+	ToggleActionMenu()
+end )
+
+
+--[[------------------------------------------------------------------------
+	ActionMenu Control and Input Blocking 
+	This is the main while loop that opens the ActionMenu on keypress. It 
+	uses the input blocking found in the ES Banking resource, credits to 
+	the authors.
+------------------------------------------------------------------------]]--
+Citizen.CreateThread( function()
+	-- This is just in case the resources restarted whilst the NUI is focused. 
+	SetNuiFocus( false )
+
+	while true do 
+		-- Control ID 20 is the 'Z' key by default 
+		-- Use https://wiki.fivem.net/wiki/Controls to find a different key 
+		if ( IsControlJustPressed( 1, 20 ) ) then 
+			ToggleActionMenu()
+		end 
+
+	    if ( menuEnabled ) then
+            local ped = GetPlayerPed( -1 )	
+
+            DisableControlAction( 0, 1, true ) -- LookLeftRight
+            DisableControlAction( 0, 2, true ) -- LookUpDown
+            DisableControlAction( 0, 24, true ) -- Attack
+            DisablePlayerFiring( ped, true ) -- Disable weapon firing
+            DisableControlAction( 0, 142, true ) -- MeleeAttackAlternate
+            DisableControlAction( 0, 106, true ) -- VehicleMouseControlOverride
+        end
+
+		Citizen.Wait( 0 )
+	end 
+end )
+
+function chatPrint( msg )
+	TriggerEvent( 'chatMessage', "ActionMenu", { 255, 255, 255 }, msg )
+end 
+
+
+
+
+
+function loadAnimDict( dict )
+    while ( not HasAnimDictLoaded( dict ) ) do
+        RequestAnimDict( dict )
+        Citizen.Wait( 5 )
+    end
+end
+ 
+RegisterNetEvent( 'KneelHU' )
+AddEventHandler( 'KneelHU', function()
+    local player = GetPlayerPed( -1 )
+    if ( DoesEntityExist( player ) and not IsEntityDead( player )) then
+        loadAnimDict( "random@arrests" )
+        loadAnimDict( "random@arrests@busted" )
+        if ( IsEntityPlayingAnim( player, "random@arrests@busted", "idle_a", 3 ) ) then
+            TaskPlayAnim( player, "random@arrests@busted", "exit", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (3000)
+            TaskPlayAnim( player, "random@arrests", "kneeling_arrest_get_up", 8.0, 1.0, -1, 128, 0, 0, 0, 0 )
+        else
+            TaskPlayAnim( player, "random@arrests", "idle_2_hands_up", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (4000)
+            TaskPlayAnim( player, "random@arrests", "kneeling_arrest_idle", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (500)
+            TaskPlayAnim( player, "random@arrests@busted", "enter", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
+            Wait (1000)
+            TaskPlayAnim( player, "random@arrests@busted", "idle_a", 8.0, 1.0, -1, 9, 0, 0, 0, 0 )
+        end    
+    end
+end )
+ 
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests@busted", "idle_a", 3) then
+            DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+            DisableControlAction(0,21,true)
+        end
+    end
+end)
+
+--Kneel Handsup End
+
+
+
+-- client side handcuff script
+
+local handCuffed = false -- store wether we are handcuffed or not
+
+RegisterNetEvent('mHandCuff') -- register that this is a valid event
+
+-- register a function to be called when we recieve the event
+AddEventHandler('mHandCuff', function()
+  -- set handCuffed equal to the oposite of its current value (true or false)
+  handCuffed = not handCuffed
+end)
+
+
+Citizen.CreateThread(function() -- create a thread
+  while true do -- loop through this code infinitely
+    Citizen.Wait(1) -- required in an infinite loop or it will crash
+
+    if (handCuffed == true) then -- we are handcuffed
+      RequestAnimDict('mp_arresting') -- tell the game to load this animation dictionary
+
+      -- check if the animation dictionary has loaded, if not, wait
+      while not HasAnimDictLoaded('mp_arresting') do
+        Citizen.Wait(0)
+      end
+
+      local myPed = PlayerPedId() -- get our ped identifier
+      local animation = 'idle' -- animation to play
+      local flags = 49 -- only play the animation on the upper body
+
+      -- play the animation
+      TaskPlayAnim(myPed, 'mp_arresting', animation, 8.0, -8, -1, flags, 0, 0, 0, 0)
+    end
+  end
+end)
